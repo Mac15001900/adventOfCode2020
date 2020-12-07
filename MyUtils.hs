4078def84588635afe10ed2c01b5be81ecbecb27
Mac15001900 <mac15001900@gmail.com>
2020-12-07 18:13:34 +0100

    Cleaned up MyUtils

--- MyUtils.hs
+++ MyUtils.hs
@@ -31,14 +31,6 @@ split p s =  case dropWhile p s of
                       s' -> w : split p s''
                             where (w, s'') = break p s'
 
-splitToLines :: String -> [String]
-splitToLines = splitToLines' ""
---              Buffer    Input     
-splitToLines' :: String -> String -> [String]
-splitToLines' b  []        = [reverse b]
-splitToLines' b  ('\n':xs) = (reverse b):(splitToLines' "" xs)
-splitToLines' b  (x:xs)    = splitToLines' (x:b) xs
-
 count :: (a->Bool) -> [a] -> Int
 count p = length . (filter p)
 
@@ -82,9 +74,4 @@ joinWith a (x:xs) = (x++a)++(joinWith a xs)
 
 valueBetween :: Ord a => (a,a) -> a -> Bool
 valueBetween (low,high) x = x >= low && x <= high
-
-intsBetween :: Int -> Int -> [Int]
-intsBetween x y = if x > y then intsBetween y x else 
-                     if x==y then [x] else
-                     x:(intsBetween (x+1) y)
     
